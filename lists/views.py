61066db3ab2699e18d5925bec26f35f297f4aabe
EimantasBlazevicius <eima.blaz@gmail.com>
2022-03-06 13:21:44 +0200

    one form done

--- lists/views.py
+++ lists/views.py
@@ -6,13 +6,26 @@ def index(request):
     list = ToDoList.objects.get(id=1)
     if request.method == "POST":
         print(request.POST)
-        checked = request.POST.getlist('checkbox')
-        for item in list.todoitems_set.all():
-            if str(item.id) in checked:
-                item.isCompleted = True
+
+        if request.POST.get('save'):
+            checked = request.POST.getlist('checkbox')
+            for item in list.todoitems_set.all():
+                if str(item.id) in checked:
+                    item.isCompleted = True
+                else:
+                    item.isCompleted = False
+                item.save()
+        elif request.POST.get('add'):
+            text = request.POST.get('new')
+            if len(text) > 3:
+                list.todoitems_set.create(text=text, isCompleted=False)
             else:
-                item.isCompleted = False
-            item.save()
+                print("Užduoties tekstas per trumpas")
+        elif request.POST.get('delete'):
+            id_to_delete = request.POST.get('delete')
+            for item in list.todoitems_set.all():
+                if str(item.id) == id_to_delete:
+                    item.delete()
         return redirect('index')
     else:
         context = {"title":f"{list.title}","items": list.todoitems_set.all() }
