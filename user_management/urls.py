cbea498a5abfb59523c926cce82a0c295ef8e7ef
EimantasBlazevicius <eima.blaz@gmail.com>
2022-03-10 15:33:15 +0200

    final commit for this branch before the lecture

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
-# accounts/ login/ [name='login']
-# accounts/ logout/ [name='logout']
+# accounts/ login/ [name='login'] +
+# accounts/ logout/ [name='logout'] +
 # accounts/ password_change/ [name='password_change']
 # accounts/ password_change/done/ [name='password_change_done']
 # accounts/ password_reset/ [name='password_reset']
@@ -9,7 +9,9 @@ from django.urls import path, include
 # accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
 # accounts/ reset/done/ [name='password_reset_complete']
 
+from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register')
 ]
--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import User
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
 
 # Create your views here.
+@csrf_protect
+def register(request):
+    if request.method == "POST":
+        # pasiimame reikšmes iš registracijos formos
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # tikriname, ar sutampa slaptažodžiai
+        if password == password2:
+            # tikriname, ar neužimtas username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, f'Vartotojo vardas {username} užimtas!')
+                return redirect('register')
+            else:
+                # tikriname, ar nėra tokio pat email
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, f'Vartotojas su el. paštu {email} jau užregistruotas!')
+                    return redirect('register')
+                else:
+                    # jeigu viskas tvarkoje, sukuriame naują vartotoją
+                    User.objects.create_user(username=username, email=email, password=password)
+                    return redirect('login')
+        else:
+            messages.error(request, 'Slaptažodžiai nesutampa!')
+            return redirect('register')
+    return render(request, 'registration/register.html')
