55fd275ac3e91b88359674c09ea17cbdb886284b
EimantasBlazevicius <eima.blaz@gmail.com>
2022-03-10 14:23:40 +0200

    basic authentication added

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>', views.list, name='list')
+    path('', login_required(views.AllUserLists.as_view()), name='home'),
+    path('<int:id>', login_required(views.list), name='list')
 ]
--- mylistsapp/urls.py
+++ mylistsapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lists.urls'))
+    path('', include('lists.urls')),
+    path('accounts/', include('user_management.urls')),
 ]
