31e5662461c9656bed1197c707d1eb972cf7a5c9
EimantasBlazevicius <eima.blaz@gmail.com>
2022-03-12 10:18:43 +0200

    auth config 1

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ToDoList(models.Model):
     title = models.CharField('Pavadinimas', max_length=100, help_text="Pavadink šitą ToDo listą kaip nors originaliai")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = 'ToDo Listas'
