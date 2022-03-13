dde633c9e6cc208d98467ab1056e9e96360fe791
EimantasBlazevicius <eima.blaz@gmail.com>
2022-03-13 11:31:05 +0200

    added api app

--- mylistsapp/urls.py
+++ mylistsapp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lists.urls')),
-    path('accounts/', include('user_management.urls'))
+    path('accounts/', include('user_management.urls')),
+    path('api/', include('api.urls')),
 ]
